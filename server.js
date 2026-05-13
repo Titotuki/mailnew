570cc99d3eec83c85b2de6d30cf1d764e3b4cef2
Titotuki <titotuktuki@gmail.com>
2026-05-13 15:21:44 +0530

    updated server file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cron = require("node-cron");
 const { Resend } = require("resend");
-const resend = new Resend("re_6ySeUKiJ_7TKtGTEHaPJwvbNQRfHp8MP9");
+//const resend = new Resend("re_6ySeUKiJ_7TKtGTEHaPJwvbNQRfHp8MP9");
+const resend = new Resend(process.env.RESEND_API_KEY);
 const app = express();
 
 cron.schedule("0 */1 * * *", async () => {
